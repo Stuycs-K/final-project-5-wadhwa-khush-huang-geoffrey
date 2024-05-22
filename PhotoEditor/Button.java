cb5ef087e52a3c1539203c087c1fa8c885d493ca
Khush Wadhwa <kwadhwa50@stuy.edu>
2024-05-22 13:42:40 -0400

    Moved File copying to import() method, still working on Button constructor

--- PhotoEditor/Button.java
+++ PhotoEditor/Button.java
@@ -1,10 +1,4 @@
 import processing.core.PImage;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 public class Button {
   PImage icon;
   private int width, height;
@@ -13,17 +7,7 @@ public class Button {
   private char keybind;
 
   public Button(String iconPath, int w, int h, String n, char bind) {
-    //Copying image to data and creating a PImage with the relevant qualities
-    File i = new File(iconPath);
-    Path in = i.toPath();
-    Path out = new File("data/" + i.getName()).toPath();
-    String target = "";
-    try {
-      target = Files.copy(in, out, StandardCopyOption.REPLACE_EXISTING).toString();
-    }
-    catch(IOException e) {}
-    icon = PImage.loadImage(i.getName());
-
+    icon = loadImage(iconPath);
     width = w;
     height = h;
     name = n;
