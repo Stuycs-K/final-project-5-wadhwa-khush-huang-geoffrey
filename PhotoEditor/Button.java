ec93be67a3d208b4696fcaceaa69745013ccb4a5
Khush Wadhwa <kwadhwa50@stuy.edu>
2024-05-22 13:14:55 -0400

    loadImage(String) doesn't exist??

--- PhotoEditor/Button.java
+++ PhotoEditor/Button.java
@@ -1,9 +1,9 @@
 import processing.core.PImage;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.io.File;
-import java.io.FilenameFilter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 public class Button {
   PImage icon;
@@ -15,8 +15,14 @@ public class Button {
   public Button(String iconPath, int w, int h, String n, char bind) {
     //Copying image to data and creating a PImage with the relevant qualities
     File i = new File(iconPath);
-    String target = Files.copy(i.getPath(), "data/" + i.getName(), StandardCopyOption.COPY_ATTRIBUTES);
-    icon = loadImage(target);
+    Path in = i.toPath();
+    Path out = new File("data/" + i.getName()).toPath();
+    String target = "";
+    try {
+      target = Files.copy(in, out, StandardCopyOption.REPLACE_EXISTING).toString();
+    }
+    catch(IOException e) {}
+    icon = PImage.loadImage(i.getName());
 
     width = w;
     height = h;
